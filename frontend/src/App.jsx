97629a2572d11e7875fcbb21fd39a1d43399c238
infinitechapters <khushiiyer14@gmail.com>
2026-03-01 23:00:01 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Dashboard from './Dashboard.jsx'
 import StudentDashboard from './pages/student/StudentDashboard.jsx'
 import { Router, Route, Routes } from 'react-router-dom'
 import LoginPage from './Login.jsx'
+import StudentAnnouncements from './pages/student/StudentAnnouncement.jsx'
+import ExtensionRequest from './pages/student/ExtensionRequest.jsx'
 
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/student/dashboard" element={<StudentDashboard />} />
+          <Route path="/student/announcements" element={<StudentAnnouncements />} />
+          <Route path="/extensionrequest" element={<ExtensionRequest />} />
           {/* <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
         </Routes>
     </>
