78fdd4dcdf9333cbf6a1fe27013fa003af441a93
infinitechapters <khushiiyer14@gmail.com>
2026-03-01 22:24:46 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import React, { useState} from 'react'
-import Dashboard from './Dahsboard'
+import React, { useState } from 'react'
+import Dashboard from './Dashboard.jsx'
+import StudentDashboard from './pages/student/StudentDashboard.jsx'
+import { Router, Route, Routes } from 'react-router-dom'
+import LoginPage from './Login.jsx'
 
 
 function App() {
- 
+
 
   return (
     <>
-     <Dashboard/>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/student/dashboard" element={<StudentDashboard />} />
+          {/* <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
+        </Routes>
     </>
   )
 }
