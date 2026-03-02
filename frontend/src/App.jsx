0656344b978e13821c283e4c89da280b3074ca6b
infinitechapters <khushiiyer14@gmail.com>
2026-03-02 10:03:07 +0530

    changes new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Dashboard from './Dashboard.jsx'
 import StudentDashboard from './pages/student/StudentDashboard.jsx'
 
@@ -8,29 +8,29 @@ import LoginPage from './Login.jsx'
 import StudentAnnouncements from './pages/student/StudentAnnouncement.jsx'
 import ExtensionRequest from './pages/student/ExtensionRequest.jsx'
 
-import Dashboard from "./pages/admin/Dashboard";
-import Students from "./pages/admin/Students";
-import Complaints from "./pages/admin/Complaints";
+import AdminDashboard from "./pages/admin/AdminDashboard.jsx";
+import Students from "./pages/admin/Students.jsx";
+import Complaints from "./pages/admin/Complaints.jsx";
+import Announcements from "./pages/admin/Annoucements.jsx";
 
 function App() {
   return (
     <>
         <Routes>
-         <Route path="/admin/dashboard" element={<Dashboard />} />
+         <Route path="/admin/dashboard" element={<AdminDashboard />} />
 
       <Route path="/admin/students" element={<Students />} />
+      <Route path="/admin/announcements" element={<Announcements />} />
 
       <Route path="/admin/complaints" element={<Complaints />} />
       <Route path="/" element={<Dashboard />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/student/dashboard" element={<StudentDashboard />} />
           <Route path="/student/announcements" element={<StudentAnnouncements />} />
-          <Route path="/extensionrequest" element={<ExtensionRequest />} />
-          {/* <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
+          <Route path="/student/extensionRequest" element={<ExtensionRequest />} />
         </Routes>
     </>
   )
-
 }
 
 export default App;
